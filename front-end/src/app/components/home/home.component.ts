e370e8e1a4535b3864bcf0398c75e6ea84580574
Qasim Khan <78582000+qasimk2120@users.noreply.github.com>
2024-03-05 18:32:50 +0300

    new changes
    new changes

--- front-end/src/app/components/home/home.component.ts
+++ front-end/src/app/components/home/home.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
-import {Router, RouterLink} from "@angular/router";
+import {Component, OnInit} from '@angular/core';
+import {RouterLink, RouterModule} from "@angular/router";
 import {NotificationComponent} from "../notification/notification.component";
 import {FooterComponent} from "../footer/footer.component";
+import {AuthService} from "../../services/auth.service";
+import {Observable} from "rxjs";
+import {AsyncPipe, CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-home',
@@ -9,11 +12,20 @@ import {FooterComponent} from "../footer/footer.component";
   imports: [
     RouterLink,
     NotificationComponent,
-    FooterComponent
+    FooterComponent,
+    AsyncPipe,
+    CommonModule
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export default class HomeComponent {
+export default class HomeComponent implements OnInit {
+  isLoggedIn$!: Observable<boolean>;
+  constructor(private authService: AuthService){
+
+  }
+  ngOnInit() {
+    this.isLoggedIn$ = this.authService.isLoggedIn$;
+  }
 
 }
